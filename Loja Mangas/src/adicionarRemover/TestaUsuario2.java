e98bc8bd58c27a3bd95618c74e00b7ddf0768925
Thais Gonçalves <thais.gosil@gmail.com>
2020-10-19 23:26:02 -0300

    versao 1

---
+++ Loja Mangas/src/adicionarRemover/TestaUsuario2.java
@@ -0,0 +1,17 @@
+package adicionarRemover;
+
+public class TestaUsuario2 {
+
+	public static void main(String[] args) {
+		
+		Usuario user = new Usuario();
+		
+		user.telaInicial();
+		
+		Cliente cliente1 = new Cliente();
+		
+		cliente1.escolheLoginouCatalogo();
+		
+	}
+
+}
---
+++ Loja Mangas/src/module-info.java
@@ -0,0 +1,2 @@
+module lojaMangas {
+}
